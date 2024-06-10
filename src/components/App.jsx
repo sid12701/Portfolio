b4b4f4e727c7c50251a83756dfc5dacbbb626bd4
Siddhant <78287897+sid12701@users.noreply.github.com>
2024-06-10 16:21:08 +0530

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,28 +5,7 @@ import About from './About';
 import Contact from './Contact';
 import Footer from './Footer';
 import Card from './Card';
-// window.addEventListener('scroll', () => {
-//   const section1 = document.getElementById('1');
-//   const section2 = document.getElementById('2');
-//   const section3 = document.getElementById('3');
 
-//   if (window.scrollY < section2.offsetTop) {
-//     // show first section
-//     section1.style.display = 'block';
-//     section2.style.display = 'none';
-//     section3.style.display = 'none';
-//   } else if (window.scrollY < section3.offsetTop) {
-//     // show second section
-//     section1.style.display = 'none';
-//     section2.style.display = 'block';
-//     section3.style.display = 'none';
-//   } else {
-//     // show third section
-//     section1.style.display = 'none';
-//     section2.style.display = 'none';
-//     section3.style.display = 'block';
-//   }
-// });
 
 
 const App = () => {
